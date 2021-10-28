7b29bd8f6db258e64b648c0a684154a8c6bfe1d5
weinber_robert <robert.weinber@gmail.com>
2021-10-28 17:36:11 +0200

    Expense Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from "./components/Expenses";
 function App() {
+
+  const expense_data = [
+    { title: "Oculus", amount: 230001, date: new Date(2021, 10, 5) },
+    { title: "Switch", amount: 130001, date: new Date(2020, 3, 2) },
+    { title: "Books", amount: 50236, date: new Date(2021, 1, 15) },
+    { title: "Games", amount: 50236, date: new Date(2021, 11, 15) },
+  ];
+
   return (
     <div>
       <h2>Header</h2>
-      <ExpenseItem></ExpenseItem>
+      <Expenses items={expense_data}/>
     </div>
   );
 }
