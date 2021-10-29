954ded6fe6cc159f3a7fc6d90480f3c7ccb7a25f
weinber_robert <robert.weinber@gmail.com>
2021-10-29 10:52:17 +0200

    Listing

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,11 +14,19 @@ function Expenses(props){
 
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={selectedFilter} onChangeDate = {changeFilterHandler}/>
-      <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}/>
-      <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}/>
-      <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}/>
-      <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}/>
+      <ExpensesFilter
+        selected={selectedFilter}
+        onChangeDate={changeFilterHandler}
+      />
+      {props.items.map(item => (
+        
+          <ExpenseItem
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        
+      ))}
     </Card>
   );
 
